708764fbfc9a8bb2928c7aef243a1234e4f9c6cf
Kyle-Hoekstra <kyle.m.s.hoekstra@gmail.com>
2024-04-15 21:05:13 -0700

    Lightning documentation

--- Assets/Scripts/Lightning.cs
+++ Assets/Scripts/Lightning.cs
@@ -15,9 +15,13 @@ public class Lightning : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //how often update will run
         perFrame = 20;
+        //Maximum intensity of my lightning light
         maxIntensity = 4f;
+        //what percentage I want my linear interpolation to go to
         percentageLight = .75f;
+        //At what intensity of light should I end the lightning strike
         lightOff = percentageLight * maxIntensity;
     }
 
@@ -26,9 +30,11 @@ public class Lightning : MonoBehaviour
     // Update is called once per 30 frames
     void Update()
     {
+        //random number to randomly lightning strike
         float strike = Random.Range(0f, 10f);
 
         if((Time.frameCount % perFrame == 0) && (strike < 2.1f)){
+            
             if(GetComponent<Light>().intensity < lightOff){
                 GetComponent<Light>().intensity = Mathf.Lerp(GetComponent<Light>().intensity, maxIntensity, percentageLight);
             } else {
