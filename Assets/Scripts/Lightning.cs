15203a87861aa3032dc4ca695aefab6d459f500e
Kyle-Hoekstra <kyle.m.s.hoekstra@gmail.com>
2024-04-15 19:46:02 -0700

    Added random lightning flash effect using linear interpolation

--- Assets/Scripts/Lightning.cs
+++ Assets/Scripts/Lightning.cs
@@ -1,20 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Mathf.lerp;
-using Time.deltaTime;
+
+
 
 public class Lightning : MonoBehaviour
 {
+
+    int perFrame;
+    float maxIntensity;
+    float percentageLight;
+    float lightOff;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        perFrame = 20;
+        maxIntensity = 4f;
+        percentageLight = .75f;
+        lightOff = percentageLight * maxIntensity;
     }
 
-    // Update is called once per frame
+
+
+    // Update is called once per 30 frames
     void Update()
     {
-        light.intensity = Mathf.lerp(light.intensity, 8f, 0.5f * Time.deltaTime);
+        float strike = Random.Range(0f, 10f);
+
+        if((Time.frameCount % perFrame == 0) && (strike < 2.1f)){
+            if(GetComponent<Light>().intensity < lightOff){
+                GetComponent<Light>().intensity = Mathf.Lerp(GetComponent<Light>().intensity, maxIntensity, percentageLight);
+            } else {
+                GetComponent<Light>().intensity = Mathf.Lerp(0f, GetComponent<Light>().intensity, 0.1f);
+            }
+        } 
     }
 }
